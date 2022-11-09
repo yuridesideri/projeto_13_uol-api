ac2640e7a8cfc82672c37f9fcc7b3449a306c59b
Yuri <yuridesideri12@gmail.com>
2022-11-09 16:06:03 -0300

    feat: messages get & post

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { client, usersCol, messagesCol } from './database.js';
+import dayjs from "dayjs";
 
 
 const server = express();
@@ -32,7 +33,29 @@ server.get('/participants',
     } 
 )
 
+server.post('/messages', 
+    async (req, res) =>{
+        const time = dayjs().locale('pt-br').format('HH:mm:ss');
+        try {
+            messagesCol.insertOne({...req.body, from: req.headers.user, time: time})
+            res.sendStatus(200);
+        } catch {
+            res.sendStatus(400);
+        }
+    }
+)
 
+server.get('/messages', async (req, res) =>{
+    const limit = parseInt(req.query.limit);
+    //limit not applied yet!!
+    try {
+        const query =  await messagesCol.find().toArray();
+        res.status(200);
+        res.send(query);
+    } catch {
+        res.sendStatus(400);
+    }
+})
 
 
 
