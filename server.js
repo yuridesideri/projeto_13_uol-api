24142eb8e9defcee41c498e0e858bd1089c91a6b
Yuri <yuridesideri12@gmail.com>
2022-11-09 15:17:27 -0300

    feat: participants get & post

--- server.js
+++ server.js
@@ -4,13 +4,35 @@ import { client, usersCol, messagesCol } from './database.js';
 
 
 const server = express();
+server.listen(5000);
 server.use(cors());
+server.use(express.json());
+
+// 
+server.post('/participants', 
+    async (req, res) => {
+        try {
+            await usersCol.insertOne({...req.body, lastStatus: Date.now()})
+            res.sendStatus(200);
+        } catch{
+            res.sendStatus(400);
+        }
+    }
+)
+
+server.get('/participants', 
+    async  (req, res) => {
+        try{
+            const users = await usersCol.find().toArray();
+            res.statusCode = 201;
+            res.send(users);
+        } catch {
+            res.sendStatus(400);
+        }
+    } 
+)
 
 
 
 
 
-
-
-
-client.close();
